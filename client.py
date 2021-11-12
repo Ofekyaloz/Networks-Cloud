bfea1d1fcbf32a496516f798f47aa733118f052e
Ofek <ofek.yaloz@live.biu.ac.il>
2021-11-12 14:52:33 +0200

    id

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 12345))
-s.send("Noam Cohen 209311620 and Ofek Yaloz 206666729")
+s.send("Noam Cohen and Ofek Yaloz")
+data = s.recv(100)
+print("Server sent: ", data)
+s.send("Noam Cohen and Ofek Yaloz")
 data = s.recv(100)
 print("Server sent: ", data)
 s.close()
