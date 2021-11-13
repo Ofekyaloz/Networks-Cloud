f25adce14ededa7d411a0f0e3a2c434ef5064703
Noam Cohen <noam60@gmail.com>
2021-11-13 23:09:53 +0200

    updated

--- client.py
+++ client.py
@@ -1,10 +1,87 @@
-import socket
+import socket, sys, os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+INVALID = -1
+IP_INDEX = 1
+PORT_INDEX = 2
+PATH_INDEX = 3
+TIME_INTERVAL_INDEX = 4
+ID_INDEX = 5
+
+
+# function that checks validity of the parameters.
+def arguments_check():
+    if len(sys.argv) < 5 or len(sys.argv) > 6:
+        print("Error: invalid number of arguments")
+        return INVALID
+    ip = sys.argv[IP_INDEX]
+    port = sys.argv[PORT_INDEX]
+    if not port.isdigit():
+        print(f'Error: invalid port: {port}')
+        return INVALID
+
+    time_interval = sys.argv[TIME_INTERVAL_INDEX]
+    if not time_interval.isdigit():
+        print(f'Error: time interval: {sys.argv[TIME_INTERVAL_INDEX]} ,is not a number')
+        return INVALID
+    else:
+        time_interval = int(time_interval)
+        if time_interval <= 0:
+            print(f'time interval {time_interval} is not valid')
+            return INVALID
+    if not os.path.isdir(sys.argv[PATH_INDEX]):
+        print(f'Error: path: {sys.argv[PATH_INDEX]} ,does not exists')
+        return INVALID
+    else:
+        pass
+    parts = ip.split('.')
+    # if len(parts) != 4:
+    #     print(f'IP: {ip} is not valid')
+    #     return INVALID
+    # i = 0
+    # for part in parts:
+    #     if not part.isdigit() or not 0 <= int(part) <= 255 or (i == 0 and int(part) == 0):
+    #         print(f'IP: {ip} is not valid')
+    #         return INVALID
+    #     i += 1
+
+
+class FileChangedHandler(FileSystemEventHandler):
+    def alert_file_modified(self, e):
+        print(f'{e.event_type}, {e.src_path}')
+
+
+flag = arguments_check()
+if flag == INVALID:
+    exit(INVALID)
+ip = sys.argv[IP_INDEX]
+port = int(sys.argv[PORT_INDEX])
+dir_path = sys.argv[PATH_INDEX]
+time_interval = sys.argv[TIME_INTERVAL_INDEX]
+client_id = ""
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.68.113', 12345))
-s.send(b'Noam Cohen and Ofek Yaloz')
-data = s.recv(100)
-print("Server sent: ", data)
-s.send(b'209311620, 206666729')
-data = s.recv(100)
-print("Server sent: ", data)
+s.connect((ip, port))
+
+if len(sys.argv) == 6:
+    client_id = sys.argv[5]
+else:
+    s.send("REGS")
+    data = s.recv(132)
+    print("Server sent: ", data)
+    client_id = data[4:]
+
+handler = FileChangedHandler()
+observer = Observer()
+observer.schedule(handler, path=dir_path, recursive = True)
+
+try:
+    while True:
+        time.sleep(2)
+except:
+    observer.stop()
+
+observer.join()
 s.close()
