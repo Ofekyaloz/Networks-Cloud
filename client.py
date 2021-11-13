1789bc4b34476e07d29220e058081620a4c49e34
Noam Cohen <noam60@gmail.com>
2021-11-13 21:19:21 +0200

    scheduled commit

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 12345))
+s.connect(('192.168.68.113', 12345))
 s.send(b'Noam Cohen and Ofek Yaloz')
 data = s.recv(100)
 print("Server sent: ", data)
