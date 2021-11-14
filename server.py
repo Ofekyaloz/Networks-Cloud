478aef8104d218a7b17bf32a7a6c35f5619110cc
Ofek <ofek.yaloz@live.biu.ac.il>
2021-11-14 22:02:47 +0200

    ids

--- server.py
+++ server.py
@@ -1,12 +1,30 @@
-import socket
+
+import socket, sys
+import os
+
+PORT_INDEX = 1
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 12345))
+server.bind(('', int(sys.argv[PORT_INDEX])))
 server.listen(5)
+clients = {}
+
 while True:
     client_socket, client_address = server.accept()
-print('Connection from: ', client_address)
-data = client_socket.recv(100)
-print('Received: ', data)
-client_socket.send(data.upper())
-client_socket.close()
-print('Client disconnected')
+    print('Connection from: ', client_address)
+    data = client_socket.recv(100)
+    request_type = data[0:4]
+
+    print('Received: ', data)
+
+    # creates a new client
+    if data != "REGS":
+        client_id = os.urandom(128)
+        clients[client_id] = []
+        client_socket.send(client_id)
+        continue
+
+    clients[client_id].append(client_socket)
+    client_socket.send("DONE" + client_id)
+
+    client_socket.close()
+    print('Client disconnected')
